109e40ab683e77eec6146ec242c5a74802ab51dd
SuptiM <supti16mukherjee@gmail.com>
2018-02-04 22:36:24 +0530

    More New files n folders

---
+++ contact/script.js
@@ -0,0 +1,12 @@
+
+$("#hedbel").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#bel").offset().top
+    }, 1000);
+});
+
+$("#heddon").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#don").offset().top
+    }, 1000);
+});
